1b813dd094b2387370d2c2ec3c0275c1c31bba36
TheVerin <tomekbeben95@gmail.com>
2018-12-12 17:02:50 +0100

    I have made migrations and remove errors from models.py

--- code_core/code_main/models.py
+++ code_core/code_main/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
-class Cipher(models.Models):
-    to = models.CharField(max_length= 100)
+
+class Cipher(models.Model):
+    to = models.CharField(max_length=100)
     message = models.TextField()
-    direction = models.CharField()
+    direction = models.CharField(max_length=10)
     translaction = models.IntegerField()
     result = models.TextField()
 
     class Meta:
         db_table = 'code_main'
-
 
     def __str__(self):
         return self.to
